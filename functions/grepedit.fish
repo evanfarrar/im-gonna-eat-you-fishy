d2b50786dc3bdc04c9c501590652ddcda3ebaaad
Evan Farrar <evan@groupon.com>
2010-03-08 14:53:46 -0600

    Grep'n'edit that actually works.

--- functions/grepedit.fish
+++ functions/grepedit.fish
@@ -1,7 +1,7 @@
 function grepedit -d "git grep and edit files"
-  set -l filelist git grep $argv | cut -f 1 -d ':' | sort | uniq
+  set filelist (git grep $argv | cut -f 1 -d ':' | sort | uniq)
+  switch '{$filelist}'
 
-  switch $filelist
     case ''
     echo no files found containing $argv
 
